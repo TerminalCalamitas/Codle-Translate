962757614e8cbb43673130d34349ff3df8277f03
caleb <paracordcaleb@gmail.com>
2026-04-11 10:52:07 -0400

    Updated GUI with example code loading

--- gui/Codle_Translate.py
+++ gui/Codle_Translate.py
@@ -27,6 +27,43 @@ class Ui_MainWindow(object):
         self.GridLayout.setLayoutDirection(Qt.LayoutDirection.LeftToRight)
         self.gridLayout = QGridLayout(self.GridLayout)
         self.gridLayout.setObjectName(u"gridLayout")
+        self.InputLanguage = QComboBox(self.GridLayout)
+        self.InputLanguage.addItem("")
+        self.InputLanguage.addItem("")
+        self.InputLanguage.addItem("")
+        self.InputLanguage.addItem("")
+        self.InputLanguage.setObjectName(u"InputLanguage")
+
+        self.gridLayout.addWidget(self.InputLanguage, 0, 0, 1, 1)
+
+        self.AboveButtonSpacing = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+
+        self.gridLayout.addItem(self.AboveButtonSpacing, 1, 1, 1, 1)
+
+        self.InputBox = QTextEdit(self.GridLayout)
+        self.InputBox.setObjectName(u"InputBox")
+        self.InputBox.viewport().setProperty("cursor", QCursor(Qt.IBeamCursor))
+        self.InputBox.setAutoFillBackground(True)
+        self.InputBox.setTabStopDistance(16.000000000000000)
+
+        self.gridLayout.addWidget(self.InputBox, 1, 0, 3, 1)
+
+        self.OutputLanguage = QComboBox(self.GridLayout)
+        self.OutputLanguage.addItem("")
+        self.OutputLanguage.addItem("")
+        self.OutputLanguage.addItem("")
+        self.OutputLanguage.addItem("")
+        self.OutputLanguage.setObjectName(u"OutputLanguage")
+
+        self.gridLayout.addWidget(self.OutputLanguage, 0, 2, 1, 1)
+
+        self.RunTranslation = QPushButton(self.GridLayout)
+        self.RunTranslation.setObjectName(u"RunTranslation")
+        self.RunTranslation.setMaximumSize(QSize(149, 16777215))
+        self.RunTranslation.setCheckable(False)
+
+        self.gridLayout.addWidget(self.RunTranslation, 2, 1, 1, 1)
+
         self.OutputBox = QTextEdit(self.GridLayout)
         self.OutputBox.setObjectName(u"OutputBox")
         self.OutputBox.setEnabled(True)
@@ -39,42 +76,10 @@ class Ui_MainWindow(object):
 
         self.gridLayout.addItem(self.Belowbuttonspacing, 3, 1, 1, 1)
 
-        self.OutputLanguage = QComboBox(self.GridLayout)
-        self.OutputLanguage.addItem("")
-        self.OutputLanguage.addItem("")
-        self.OutputLanguage.addItem("")
-        self.OutputLanguage.addItem("")
-        self.OutputLanguage.setObjectName(u"OutputLanguage")
+        self.LoadExample = QPushButton(self.GridLayout)
+        self.LoadExample.setObjectName(u"LoadExample")
 
-        self.gridLayout.addWidget(self.OutputLanguage, 0, 2, 1, 1)
-
-        self.AboveButtonSpacing = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
-
-        self.gridLayout.addItem(self.AboveButtonSpacing, 1, 1, 1, 1)
-
-        self.RunTranslation = QPushButton(self.GridLayout)
-        self.RunTranslation.setObjectName(u"RunTranslation")
-        self.RunTranslation.setMaximumSize(QSize(149, 16777215))
-        self.RunTranslation.setCheckable(False)
-
-        self.gridLayout.addWidget(self.RunTranslation, 2, 1, 1, 1)
-
-        self.InputLanguage = QComboBox(self.GridLayout)
-        self.InputLanguage.addItem("")
-        self.InputLanguage.addItem("")
-        self.InputLanguage.addItem("")
-        self.InputLanguage.addItem("")
-        self.InputLanguage.setObjectName(u"InputLanguage")
-
-        self.gridLayout.addWidget(self.InputLanguage, 0, 0, 1, 1)
-
-        self.InputBox = QTextEdit(self.GridLayout)
-        self.InputBox.setObjectName(u"InputBox")
-        self.InputBox.viewport().setProperty("cursor", QCursor(Qt.IBeamCursor))
-        self.InputBox.setAutoFillBackground(True)
-        self.InputBox.setTabStopDistance(16.000000000000000)
-
-        self.gridLayout.addWidget(self.InputBox, 1, 0, 3, 1)
+        self.gridLayout.addWidget(self.LoadExample, 4, 0, 1, 1)
 
         self.gridLayout.setColumnStretch(0, 1)
         self.gridLayout.setColumnStretch(2, 1)
@@ -92,25 +97,25 @@ class Ui_MainWindow(object):
         QWidget.setTabOrder(self.RunTranslation, self.OutputBox)
 
         self.retranslateUi(MainWindow)
-        self.InputLanguage.currentTextChanged.connect(self.InputBox.setPlainText)
 
         QMetaObject.connectSlotsByName(MainWindow)
     # setupUi
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Codle Translate", None))
-        self.OutputBox.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Translated Code", None))
-        self.OutputLanguage.setItemText(0, QCoreApplication.translate("MainWindow", u"Javascript", None))
-        self.OutputLanguage.setItemText(1, QCoreApplication.translate("MainWindow", u"Python", None))
-        self.OutputLanguage.setItemText(2, QCoreApplication.translate("MainWindow", u"Java", None))
-        self.OutputLanguage.setItemText(3, QCoreApplication.translate("MainWindow", u"C", None))
-
-        self.RunTranslation.setText(QCoreApplication.translate("MainWindow", u"  Translate Input  ", None))
         self.InputLanguage.setItemText(0, QCoreApplication.translate("MainWindow", u"Javascript", None))
         self.InputLanguage.setItemText(1, QCoreApplication.translate("MainWindow", u"Python", None))
         self.InputLanguage.setItemText(2, QCoreApplication.translate("MainWindow", u"Java", None))
         self.InputLanguage.setItemText(3, QCoreApplication.translate("MainWindow", u"C", None))
 
         self.InputBox.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Code to Translate", None))
+        self.OutputLanguage.setItemText(0, QCoreApplication.translate("MainWindow", u"Javascript", None))
+        self.OutputLanguage.setItemText(1, QCoreApplication.translate("MainWindow", u"Python", None))
+        self.OutputLanguage.setItemText(2, QCoreApplication.translate("MainWindow", u"Java", None))
+        self.OutputLanguage.setItemText(3, QCoreApplication.translate("MainWindow", u"C", None))
+
+        self.RunTranslation.setText(QCoreApplication.translate("MainWindow", u"  Translate Input  ", None))
+        self.OutputBox.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Translated Code", None))
+        self.LoadExample.setText(QCoreApplication.translate("MainWindow", u"Load Example Code", None))
     # retranslateUi
 
